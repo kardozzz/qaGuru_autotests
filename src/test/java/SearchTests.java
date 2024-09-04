924d03c4f756af44427e90fcdeb9dd8b9d1c5f14
alabzin <kard33@mail.ru>
2024-09-04 11:32:07 +0300

    Update SearchTests.java
    CreateConflict

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,8 +6,9 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
-
-    Configuration.pageLoadStrategy = "eager";
+    static {
+        Configuration.pageLoadStrategy = "eager!!!";
+    }
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
